3bfc577ca388e97ea5083765d41a0d0f30333181
aat <aat@CLDBHPAAT>
2018-08-08 21:54:48 +0200

    test

---
+++ Assesment/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assesment.Models
+{
+    public class Person
+    {
+        public int GuessNumber { get; set; }
+    }
+}
